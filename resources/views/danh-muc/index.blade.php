0f4d27fea7c7bd5f93c09510882923d2238f2853
quyen <lvquyen203@gmail.com>
2023-11-20 20:07:24 +0700

    firt commit

---
+++ resources/views/danh-muc/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>hoc php</title>
+    </head>
+    <body>
+        <h1>xin chào mọi người đến với danh mục</h1>
+    </body>
+    </html>
---
+++ resources/views/danh-muc/tao-danh-muc/new.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>hoc php</title>
+    </head>
+    <body>
+        <h1>xin chào mọi người đến với tao danh mục</h1>
+    </body>
+    </html>
